006098e38bce11c6e747069d6c82b78207038f00
gozde <gozdeunveer@gmail.com>
2023-06-10 12:31:01 +0200

    m

--- kabsch-umeyama.py
+++ kabsch-umeyama.py
@@ -2,26 +2,25 @@ import numpy as np
 import open3d as o3d
 import meshio
 
-def kabsch_umeyama(A, B):
+def kabsch_umeyama(A, B,scale=True):
     assert A.shape == B.shape
     n, m = A.shape
-
+    print(n,m)
+###########
     EA = np.mean(A, axis=0)
     EB = np.mean(B, axis=0)
-    VarA = np.mean(np.linalg.norm(A - EA, axis=1) ** 2)
+    VarA = np.var(A, axis=0).sum()
 
-    H = ((A - EA).T @ (B - EB)) / n
+    H = ((A - EA).T @ (B - EB)) 
     U, D, VT = np.linalg.svd(H)
     d = np.sign(np.linalg.det(U) * np.linalg.det(VT))
-    S = np.diag([1] * m)
-    
-    if d<0:
-        S[-1,-1]=d
+    S = np.diag([1] * (m - 1) + [d])
 
-    R = U @ S @ VT
-    #R = U @ VT
-    c = VarA / np.trace(np.diag(D) @ S)
-    t = EA - c * R @ EB
+    R = VT @ S @ U
+    c =VarA / np.trace(np.diag(D) @ S)
+    t = EA - R @ EB.T
+
+
     return R, c, t
 
 
@@ -32,14 +31,12 @@ A_correspondence=np.load("./data/mesh1_correspondences.npy")
 B_correspondence=np.load("./data/mesh2_correspondences.npy")
 R, c, t = kabsch_umeyama(A_correspondence, B_correspondence)
 print(R,c,t)
-B = np.asarray(o3d.io.read_triangle_mesh("./leo_meshes/toy_deformed/toy_deformed.obj",enable_post_processing=True).vertices)
 
-#B=meshio.read("./leo_meshes/toy_deformed/toy_deformed.obj")
+#B_pcd_updated = np.array([t+ R @ b for b in B_correspondence])
+B_pcd_updated=np.matmul(B_correspondence,R.T)+t
 
-
-B_pcd_updated = np.array([t + c *R @ b for b in B_correspondence])
 pcd = o3d.geometry.PointCloud()
 pcd.points = o3d.utility.Vector3dVector(B_pcd_updated)
-o3d.io.write_point_cloud("./leo_meshes/temp.ply", pcd)
+o3d.io.write_point_cloud("temp.ply", pcd)
 
 
