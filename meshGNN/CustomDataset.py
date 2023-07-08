5319c89ffc0f77dc41a323fb29819b0e6f21d1fd
Askar Kolushev <19ak97@gmail.com>
2023-07-08 21:25:41 +0200

    Cleanup

--- meshGNN/CustomDataset.py
+++ meshGNN/CustomDataset.py
@@ -3,65 +3,6 @@ from pathlib import Path
 import numpy as np
 import torch
 import open3d as o3d
-import pymesh as pm
-
-from scipy.spatial.distance import cdist
-
-
-def assert_intersection_size(arr1, arr2):
-    # https://stackoverflow.com/questions/8317022/get-intersecting-rows-across-two-2d-numpy-arrays
-    _, ncols = arr1.shape
-    dtype={'names':['f{}'.format(i) for i in range(ncols)],
-        'formats':ncols * [arr1.dtype]}
-    common_points = np.intersect1d(arr1.view(dtype), arr2.view(dtype))
-    common_points = common_points.view(arr1.dtype).reshape(-1, ncols)
-    assert common_points.shape == arr1.shape
-
-
-def compute_v_merge_map(mesh_points, cloud_points):
-    mesh_v_merge_map = np.zeros(len(mesh_points), dtype=int)
-    distances = cdist(mesh_points, cloud_points)
-    mesh_v_merge_map = distances.argmin(axis=1)
-    assert len(np.unique(mesh_v_merge_map, axis=0)) == len(cloud_points)
-
-
-def replace_vertices_in_mesh(mesh, points, in_place):
-    mesh_points = o3d.utility.Vector3dVector(points)
-    if in_place:
-        mesh.vertices = mesh_points
-        return mesh
-    else:
-        return o3d.geometry.TriangleMesh(mesh_points, mesh.triangles)
-
-
-def remove_duplicate_vertices(mesh, in_place):
-    pm_mesh = pm.form_mesh(np.asarray(mesh.vertices), np.asarray(mesh.triangles))
-    pm_mesh = pm.remove_duplicated_vertices(pm_mesh)
-
-    vertices = o3d.utility.Vector3dVector(pm_mesh.vertices)
-    faces = o3d.utility.Vector3iVector(pm_mesh.faces)
-    if in_place:
-        mesh.vertices = vertices
-        mesh.triangles = faces
-        return mesh
-    else:
-        return o3d.geometry.TriangleMesh(vertices, faces)
-
-
-def pc_to_mesh(mesh, point_cloud, in_place=True, assert_shapes=False):
-    mesh_points = np.asarray(mesh.vertices)
-    cloud_points = np.asarray(point_cloud.points)
-
-    if assert_shapes:
-        assert_intersection_size(mesh_points, cloud_points)
-
-    mesh_v_merge_map = compute_v_merge_map(mesh_points, cloud_points)
-    mesh_points = cloud_points[mesh_v_merge_map]
-    mesh = replace_vertices_in_mesh(mesh, mesh_points, in_place)
-    mesh = remove_duplicate_vertices(mesh, in_place)    
-    assert len(mesh.vertices) == len(cloud_points)
-
-    return mesh
 
 
 class CustomDataset(torch.utils.data.Dataset):
