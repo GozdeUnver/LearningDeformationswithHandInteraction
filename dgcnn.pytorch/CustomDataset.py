8a817b92f5726c9b8189da3b2ba4c67da3876961
Gözde Ünver <gozdeunveer@gmail.com>
2023-07-12 12:21:30 +0200

    created new cube dataset

--- dgcnn.pytorch/CustomDataset.py
+++ dgcnn.pytorch/CustomDataset.py
@@ -14,24 +14,35 @@ class CustomDataset(torch.utils.data.Dataset):
         super().__init__()
         df_temp=pd.read_csv("data/cube_dataset.csv")
         if split=="train":
-            df=df_temp.loc[df_temp['train_sample'] == True]
+            df=df_temp.loc[df_temp['set'] == "train"]
+        elif split=="val":
+            df=df_temp.loc[df_temp['set'] == "val"]
         else:
-            df=df_temp.loc[df_temp['train_sample'] != True]
+            df=df_temp.loc[df_temp['set'] == "test"]
         #df.drop(columns=["Unnamed: 0"],inplace=True)
         df.reset_index(drop=True,inplace=True)
-        self.input_paths=df.input.tolist()
+        self.input_paths=df["input"].tolist()
         self.target_paths=df["target"].tolist()
         self.contact_points=df["contact_vertex"].astype(int).tolist()
+
+        self.data = []
+        for i in range(len(self)):
+            if i % 100 == 0:
+                print(f'\rPre-loading dataset: {i}/{len(self)}')
+            self.data.append(self._load_item(i))
 
         #self.num_points=num_points
         self.seg_num_all = 3
         self.seg_start_index = 0
             
     def __getitem__(self, index):
+        return self.data[index] if self.data is not None else self._load_item(index)
+
+    def _load_item(self, index):
         #index = 0 # THIS IS BECAUSE OF BATCHNORMS - CHANGE WHEN NOT OVERFITTING!
         
-        input_mesh=np.asarray(pv.read(self.input_paths[index]).points,dtype=np.float16)
-        target_mesh=np.asarray(pv.read(self.target_paths[index]).points, dtype=np.float16)
+        input_mesh=np.asarray(pv.read(self.input_paths[index]).points,dtype=np.float32)
+        target_mesh=np.asarray(pv.read(self.target_paths[index]).points, dtype=np.float32)
         contact_point=self.contact_points[index]
        
         
